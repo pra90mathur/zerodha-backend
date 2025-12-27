a8d3c3e5c2f6d7e7e7d2b3045fd3f4b911cede44
Prajjwal Mathur <pra90mathur@gmail.com>
2025-12-27 14:52:37 +0530

    fix

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 require("dotenv").config();
 const express = require("express");
 const mongoose = require("mongoose");
+const path = require("path");
 
 const bodyParser = require("body-parser");
 const cors = require("cors");
