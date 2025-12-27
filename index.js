96432ad6a227cdab84e53170f9cc4a15c549fae2
Prajjwal Mathur <pra90mathur@gmail.com>
2025-12-27 14:56:09 +0530

    fix

--- index.js
+++ index.js
@@ -99,7 +99,7 @@ app.post("/newOrder", async (req, res) => {
 
 app.use(express.static(path.join(__dirname, "../dashboard/build")));
 
-app.get("*", (req, res) => {
+app.get("/*", (req, res) => {
   res.sendFile(path.join(__dirname, "../dashboard/build", "index.html"));
 });
 
